6183ba014436a653520fc6a35612a7c0c1ac19c3
Lion-09 <lion.hoti@icloud.com>
2025-09-29 14:11:48 +0200

    aufgabe 10  begonnen

---
+++ Aufgabe 10/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Aufgabe_10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Bitte geben Sie eine Zahl ein: "); 
+            int zahl = Convert.ToInt32(Console.ReadLine());
+
+            int sum = 0;
+            while (zahl != 0)
+            {
+                sum = sum + (zahl % 10); 
+                zahl = zahl / 10;        
+            }
+
+            Console.WriteLine("Die Ziffernsumme ist: " + sum);
+            Console.ReadLine();
+        } 
+
